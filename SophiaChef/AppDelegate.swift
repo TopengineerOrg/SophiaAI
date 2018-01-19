1a8a0f42925ef90d952240f30e67c3f5ddcc17f3
galileoguzman <galileoguzman@gmail.com>
2018-01-19 04:01:28 -0600

    Sophia the chef working

--- SophiaChef/AppDelegate.swift
+++ SophiaChef/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ApiAI
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = AIDefaultConfiguration()
+        configuration.clientAccessToken = ""
+        
+        let apiai = ApiAI.shared()
+        apiai?.configuration = configuration
+        
         return true
     }
 
--- SophiaChef/ViewController.swift
+++ SophiaChef/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
+import ApiAI
 
 class ViewController: UIViewController {
 
+    // PROPERTIES
+    let speechSynthesizer = AVSpeechSynthesizer()
+    @IBOutlet weak var lblResponse: UILabel!
+    @IBOutlet weak var txtMessage: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.initController()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    // SELF METHODS
+    private func initController(){
+        self.speechAndText(text: "Welcome, I'm Sophia the Chef")
+    }
+    
+    func speechAndText(text: String) {
+        let speechUtterance = AVSpeechUtterance(string: text)
+        speechSynthesizer.speak(speechUtterance)
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveEaseInOut, animations: {
+            self.lblResponse.text = text
+        }, completion: nil)
+    }
+    
+    @IBAction func btnSendMessage(_ sender: Any) {
+        let request = ApiAI.shared().textRequest()
+        
+        if let text = self.txtMessage.text, text != "" {
+            request?.query = text
+        } else {
+            return
+        }
+        
+        request?.setMappedCompletionBlockSuccess({ (request, response) in
+            let response = response as! AIResponse
+            if let textResponse = response.result.fulfillment.speech {
+                self.speechAndText(text: textResponse)
+            }
+        }, failure: { (request, error) in
+            print(error!)
+        })
+        
+        ApiAI.shared().enqueue(request)
+        txtMessage.text = ""
+        
+        
+    }
 
 }
 
