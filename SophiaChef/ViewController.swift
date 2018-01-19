9bedc2218f4ac559461ef38b38351f345f085b19
galileoguzman <galileoguzman@gmail.com>
2018-01-19 01:07:28 -0600

    Initial Commit

---
+++ SophiaChef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SophiaChef
+//
+//  Created by Galileo Guzman on 1/19/18.
+//  Copyright © 2018 Galileo Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
